edcb9c1e4e641f5d9e18dba2b95ca017b851f6df
SubKartik <kartik@vastdata.com>
2020-04-23 03:34:11 +0000

    Updated for RAPLAB

--- start-dali.sh
+++ start-dali.sh
@@ -2,4 +2,4 @@
 # Start the NVIDIA standard build for their deep learning software.
 # Includes tensorflow-gpu. python3, NVIDIA Dali, and lot of other stuff
 #
-docker run -it --shm-size=1g --ulimit memlock=-1 -v /opt/robtest:/opt/robtest nvcr.io/nvidia/tensorflow:19.12-tf1-py3  bash
+docker run -it --shm-size=1g --ulimit memlock=-1 -v ~/imagenet-scratch:/imagenet-scratch -v ~/aug:/aug  nvcr.io/nvidia/tensorflow:19.12-tf1-py3  bash
